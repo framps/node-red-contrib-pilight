4472e4b927dfb42fb38aa0f78081f6da1d8647ff
framp <framp@linux-tips-and-tricks.de>
2020-06-18 19:27:25 +0200

    Added first hard coded pollin url

--- nodes/pilight-switches.js
+++ nodes/pilight-switches.js
@@ -1,10 +1,23 @@
+
 module.exports = function(RED) {
     function PilightSwitchNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.payload = msg.payload;
-            node.send(msg);
+
+          var target="192.168.0.12:5001";
+
+          var device="protocol=pollin&systemcode=31&unitcode=4";
+          var ctl="on=1";
+          if (msg.payload === false) {
+            ctl="off=1";
+          };
+          var url="http://"+target+"/send?"+device+"&"+ctl
+          msg={
+            "url": url,
+            "payload": url
+          }
+          node.send(msg);
         });
     }
     RED.nodes.registerType("pilight-switches",PilightSwitchNode);
